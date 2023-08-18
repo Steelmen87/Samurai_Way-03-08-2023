d0ebcc032c6a3ea78b6d309b9f55abf0dbcab15c
Steel <Steelmen87@bk.ru>
2023-08-18 13:24:53 +0300

    Updates

---
+++ precache-manifest.7c4dd8ba8276e342c1db16737cd4fe95.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2bbbedb955d1fe9476a83fbae48f3c",
+    "url": "/Samurai_Way-03-08-2023/index.html"
+  },
+  {
+    "revision": "f8c2726a2b7311c8dde3",
+    "url": "/Samurai_Way-03-08-2023/static/css/4.344cb52f.chunk.css"
+  },
+  {
+    "revision": "97438917c6f92c1eab7c",
+    "url": "/Samurai_Way-03-08-2023/static/css/5.4247fd47.chunk.css"
+  },
+  {
+    "revision": "ae2f7f409e72ed23cbec",
+    "url": "/Samurai_Way-03-08-2023/static/css/6.99d45ed7.chunk.css"
+  },
+  {
+    "revision": "f0c779437404d0f58b41",
+    "url": "/Samurai_Way-03-08-2023/static/css/main.a9e806f7.chunk.css"
+  },
+  {
+    "revision": "80700b01cf4f20c5309e",
+    "url": "/Samurai_Way-03-08-2023/static/js/0.7f9463e1.chunk.js"
+  },
+  {
+    "revision": "f28416746924f63cb157",
+    "url": "/Samurai_Way-03-08-2023/static/js/3.e5a9bffc.chunk.js"
+  },
+  {
+    "revision": "f8c2726a2b7311c8dde3",
+    "url": "/Samurai_Way-03-08-2023/static/js/4.9365cdd5.chunk.js"
+  },
+  {
+    "revision": "97438917c6f92c1eab7c",
+    "url": "/Samurai_Way-03-08-2023/static/js/5.43028255.chunk.js"
+  },
+  {
+    "revision": "ae2f7f409e72ed23cbec",
+    "url": "/Samurai_Way-03-08-2023/static/js/6.ae8d6087.chunk.js"
+  },
+  {
+    "revision": "f0c779437404d0f58b41",
+    "url": "/Samurai_Way-03-08-2023/static/js/main.f5a8a78c.chunk.js"
+  },
+  {
+    "revision": "61d62b2d8e6b112eb43e",
+    "url": "/Samurai_Way-03-08-2023/static/js/runtime~main.14695ba2.js"
+  },
+  {
+    "revision": "6e7c9db7e1f100edac7c6e48714d2ea0",
+    "url": "/Samurai_Way-03-08-2023/static/media/preloader.6e7c9db7.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/Samurai_Way-03-08-2023/static/media/user.8b1dbb1f.png"
+  }
+]);
